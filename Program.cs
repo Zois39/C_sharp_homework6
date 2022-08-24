ee7ca19a8a8fda15ce1707aa4b24b5de7ae57785
konstantin bondarenko <zois1990@gmail.com>
2022-08-24 16:01:19 +0200

    task 43 complete

--- Program.cs
+++ Program.cs
@@ -2,13 +2,13 @@
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3   ***4? :)***
 
-
-int ReadNumbers()
+/*
+double ReadNumbers()
 {
-    int count = 0;
+    double count = 0;
     for(string? str = string.Empty;;)
     {
-        Console.WriteLine("Input number: ");
+        Console.Write("Input number: ");
         str = Console.ReadLine();
         if (String.IsNullOrEmpty(str))
             return count;
@@ -17,13 +17,32 @@ int ReadNumbers()
     }
 }
 Console.WriteLine($"Count positive numbers: {ReadNumbers()}");
-
-
+*/
 
 //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+/*
+double[] Coordinates (double k1, double b1, double k2, double b2)
+{
+    double[] coorDot = new double[2];
+    coorDot[0] = (b2 - b1) / (k1 - k2);
+    coorDot[1] = k1 * coorDot[0] + b1;
+    return coorDot;
+}
+double ValueCoeff (string name)
+{
+    double coeff;
+    Console.Write($"Input number {name}: ");
+    coeff = Convert.ToInt32(Console.ReadLine());
+    return coeff;
+}
+double b1 = ValueCoeff("b1");
+double k1 = ValueCoeff("k1");
+double b2 = ValueCoeff("b2");
+double k2 = ValueCoeff("k2");
 
-
-
+double[] dot = Coordinates (k1, b1, k2, b2);
+Console.Write($"Coordinates intersection point: ({dot[0]}; {dot[1]})");
+*/
